30d49b6a714a1fe5966be4afb271749e9584c345
duy <nguyenduy22071998@gmail.com>
2025-10-25 16:08:20 +0700

    refactor: Update Instagram content script to streamline feature initialization and clean up imports

--- src/content/instagram.ts
+++ src/content/instagram.ts
@@ -1,23 +1,17 @@
 import { initReelsFeature } from "./features/instagram/reels";
-import { initTimeUsageTracker } from "./features/common/timeUsage";
-import { Products } from "../utils/constants";
+// import { initTimeUsageTracker } from "./features/common/timeUsage";
+// import { Products } from "../utils/constants";
 
 async function setupInstagramContentScript() {
   console.log("🚀 Instagram Content Script Loaded");
-  
+
   // Initialize time usage tracker
-  await initTimeUsageTracker(Products.INSTAGRAM);
-  
+  // await initTimeUsageTracker(Products.INSTAGRAM);
+
   // Setup page-specific features
   const path = location.pathname;
-  switch (path) {
-    case '/reel':
-    case '/reels':
-      initReelsFeature();
-      break;
-  
-    default:
-      break;
+  if (path.includes("/reels")) {
+    initReelsFeature();
   }
 }
 
